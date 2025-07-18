d72aef601f20c1fec04ed3b8a967621462667f9a
xGp35 <girijaprasanna1999@gmail.com>
2025-07-19 00:55:52 +0530

    embeddins intro

---
+++ Embdeddings_API/test1.py
@@ -0,0 +1,9 @@
+# Create an OpenAI client
+client = ____(api_key="<OPENAI_API_TOKEN>")
+
+# Create a request to obtain embeddings
+response = ____
+
+# Convert the response into a dictionary
+response_dict = ____
+print(response_dict)
