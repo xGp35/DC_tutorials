b62b6f368efc3cd66ccdd2a5d8e07c51708c536d
xGp35 <girijaprasanna1999@gmail.com>
2025-08-26 00:38:25 +0530

    samll additon in prompt template

--- LangChain_LLM/prompt_template.py
+++ LangChain_LLM/prompt_template.py
@@ -15,3 +15,23 @@ llm_chain = prompt | llm
 # Invoke the chain on the question
 question = "How does LangChain make LLM application development easier?"
 print(llm_chain.invoke({"question": question}))
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", api_key='<OPENAI_API_TOKEN>')
+
+# Create a chat prompt template
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a geography expert that returns the colors present in a country's flag."),
+        ("human", "France"),
+        ("ai", "blue, white, red"),
+        ("human", "{country}")
+    ]
+)
+
+# Chain the prompt template and model, and invoke the chain
+llm_chain = prompt_template | llm
+
+country = "Japan"
+response = llm_chain.invoke({"country": country})
+print(response.content)
