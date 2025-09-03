5862d50281061ef6356353e227cb3cc1ab8778f1
xGp35 <girijaprasanna1999@gmail.com>
2025-09-04 00:04:45 +0530

    text analysis space removal

--- Prompt_engineering/text_analysis.py
+++ Prompt_engineering/text_analysis.py
@@ -13,13 +13,9 @@ client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
 ticket = """
 Subject: Urgent - Login Error
-
 Hi Support Team,
-
 I'm having trouble accessing my account with the username "example_user." Every time I try to log in, I encounter an error message. I've already attempted to reset my password, but the issue persists. I need to resolve this problem urgently, as I have pending tasks that require immediate attention.
-
 Please investigate and assist promptly.
-
 Thanks,
 John.
 """
