450d7712ba3319e96d6712ad08b5f7660f20d656
xGp35 <girijaprasanna1999@gmail.com>
2025-11-23 19:36:01 +0530

    classification complete

--- Supervised_Learning/train_test_split.py
+++ Supervised_Learning/train_test_split.py
@@ -7,12 +7,10 @@ churn_df = pd.read_csv("churn.csv")
 y = churn_df["churn"].values
 X = churn_df[["account_length", "customer_service_calls"]].values
 
-X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size =0.3, random_state = 21, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =0.3, random_state = 21, stratify=y)
 
 # original proportions
 unique, counts = np.unique(y, return_counts=True)
 print(dict(zip(unique, counts)))
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
 print(np.unique(y_train, return_counts=True))
 print(np.unique(y_test, return_counts=True))
