a676bf4c650e07bc709e6cc4e73730fce89db417
xGp35 <girijaprasanna1999@gmail.com>
2025-12-04 19:07:19 +0530

    commenting logistic regression

--- Supervised_Learning/logistic_regression.py
+++ Supervised_Learning/logistic_regression.py
@@ -1,5 +1,5 @@
-Building a logistic regression model
+# Building a logistic regression model
 
-In this exercise, you will build a logistic regression model using all features in the diabetes_df dataset. The model will be used to predict the probability of individuals in the test set having a diabetes diagnosis.
+# In this exercise, you will build a logistic regression model using all features in the diabetes_df dataset. The model will be used to predict the probability of individuals in the test set having a diabetes diagnosis.
 
-The diabetes_df dataset has been split into X_train, X_test, y_train, and y_test, and preloaded for you.
+# The diabetes_df dataset has been split into X_train, X_test, y_train, and y_test, and preloaded for you.
